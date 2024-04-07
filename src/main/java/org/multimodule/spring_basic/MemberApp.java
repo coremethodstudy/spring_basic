90b0a3dbc113555b34fb56097f36d60ebb9769e3
brightGarden02 <rreone2017@gmail.com>
2024-04-07 11:30:43 +0900

    feat: adapt Spring container in MemberApp

---
+++ src/main/java/org/multimodule/spring_basic/MemberApp.java
@@ -0,0 +1,21 @@
+package org.multimodule.spring_basic;
+
+import org.multimodule.spring_basic.command.application.AppConfig;
+import org.multimodule.spring_basic.command.application.MemberService;
+import org.multimodule.spring_basic.command.domain.member.Grade;
+import org.multimodule.spring_basic.command.domain.member.Member;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("member.getName() = " + member.getName());
+        System.out.println("findMember.getName() = " + findMember.getName());
+    }
+}
--- src/main/java/org/multimodule/spring_basic/command/application/AppConfig.java
+++ src/main/java/org/multimodule/spring_basic/command/application/AppConfig.java
@@ -6,13 +6,18 @@ import org.multimodule.spring_basic.repository.CarItemRepository;
 import org.multimodule.spring_basic.repository.DiscountOrderRepository;
 import org.multimodule.spring_basic.repository.ItemRepository;
 import org.multimodule.spring_basic.repository.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(new MemoryMemberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(
                 new MemoryMemberDao()
@@ -22,10 +27,12 @@ public class AppConfig {
         );
     }
 
+    @Bean
     public ItemRepository itemRepository() {
         return new CarItemRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
