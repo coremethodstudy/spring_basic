2a309c9bd49de745787708c248db45c02641e903
brightGarden02 <rreone2017@gmail.com>
2024-04-09 09:04:01 +0900

    feat: add other discountPolicy

--- src/main/java/org/multimodule/spring_basic/config/AppConfig.java
+++ src/main/java/org/multimodule/spring_basic/config/AppConfig.java
@@ -43,7 +43,8 @@ public class AppConfig {
 
     @Bean
     public DiscountPolicy discountPolicy() {
-        return new RateDiscountPolicy();
+//        return new RateDiscountPolicy();
+        return new FixDiscountPolicy();
     }
 
     @Bean
