3ee406a00fdbedd86f5ad579e26cd210d3c598c1
brightGarden02 <rreone2017@gmail.com>
2024-04-12 09:57:52 +0900

    feat: initializingBean, DisposableBean

--- src/main/java/org/multimodule/spring_basic/lifecycle/NetworkClient.java
+++ src/main/java/org/multimodule/spring_basic/lifecycle/NetworkClient.java
@@ -1,6 +1,9 @@
 package org.multimodule.spring_basic.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
@@ -25,5 +28,16 @@ public class NetworkClient {
 
     public void disconnect() {
         System.out.println("close " + url);
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
     }
 }
